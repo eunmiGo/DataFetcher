73c2ecf87fc867d157096a0e5dce8023026f17a9
eunmiGo <148742489+eunmiGo@users.noreply.github.com>
2024-11-25 18:17:10 +0900

    [load_meta_data.py 구조변경]

--- src/load/load_meta_data.py
+++ src/load/load_meta_data.py
@@ -6,43 +6,51 @@ from config.config import Config
 import psycopg2
 
 
-def load_meta_data(file_path, file_size):
-    conn = psycopg2.connect(
-        host=Config.postgres_server.host,
-        database=Config.postgres_server.database,
-        user=Config.postgres_server.user,
-        password=Config.postgres_server.password,
-        port=Config.postgres_server.port
-    )
+class LoadMetaData:
+    def __init__(self, file_path, file_size):
+        self.file_path = file_path
+        self.file_size = file_size
 
-    cur = conn.cursor()
+    def load_meta_data(self):
+        file_path = self.file_path
+        file_size = self.file_size
 
-    if isinstance(file_path, list) & isinstance(file_size, list):
-        for path, size in zip(file_path, file_size):
-            file_name = os.path.basename(path)
+        conn = psycopg2.connect(
+            host=Config.postgres_server.host,
+            database=Config.postgres_server.database,
+            user=Config.postgres_server.user,
+            password=Config.postgres_server.password,
+            port=Config.postgres_server.port
+        )
+
+        cur = conn.cursor()
+
+        if isinstance(file_path, list) & isinstance(file_size, list):
+            for path, size in zip(file_path, file_size):
+                file_name = os.path.basename(path)
+                try:
+                    insert_query = """
+                        INSERT INTO file_metadata (file_name, upload_time, file_size, file_path)
+                        VALUES (%s, %s, %s, %s)
+                    """
+                    cur.execute(insert_query, (file_name, datetime.now(), size, path))
+                    conn.commit()
+                except psycopg2.Error as db_error:
+                    print(f"PostgreSQL 저장 중 오류 발생: {db_error}")
+                    conn.rollback()
+        else:
+            file_name = os.path.basename(file_path)
             try:
                 insert_query = """
-                    INSERT INTO file_metadata (file_name, upload_time, file_size, file_path)
-                    VALUES (%s, %s, %s, %s)
-                """
-                cur.execute(insert_query, (file_name, datetime.now(), size, path))
+                        INSERT INTO file_metadata (file_name, upload_time, file_size, file_path)
+                        VALUES (%s, %s, %s, %s)
+                    """
+                cur.execute(insert_query, (file_name, datetime.now(), file_size, file_path))
                 conn.commit()
+
             except psycopg2.Error as db_error:
                 print(f"PostgreSQL 저장 중 오류 발생: {db_error}")
                 conn.rollback()
-    else:
-        file_name = os.path.basename(file_path)
-        try:
-            insert_query = """
-                    INSERT INTO file_metadata (file_name, upload_time, file_size, file_path)
-                    VALUES (%s, %s, %s, %s)
-                """
-            cur.execute(insert_query, (file_name, datetime.now(), file_size, file_path))
-            conn.commit()
-
-        except psycopg2.Error as db_error:
-            print(f"PostgreSQL 저장 중 오류 발생: {db_error}")
-            conn.rollback()
-        finally:
-            conn.close()
-    return "메타데이터가 PostgreSQL에 저장되었습니다."
+            finally:
+                conn.close()
+        return "메타데이터가 PostgreSQL에 저장되었습니다."
